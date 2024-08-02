9ea46da47a349c28583c4e3af3638ae1c6dd75fa
Mariana <mazzinoob@gmail.com>
2024-08-02 16:13:23 -0300

    ok

--- github.js
+++ github.js
@@ -15,6 +15,20 @@ servidor.get('/projeto/soma/:n1/:n2', (req, resp) => {
 
 })
 
+servidor.get('/projeto/subtrair/:n1/:n2', (req, resp) => {
+
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
+
+    let sub = n1 - n2
+
+    resp.send({
+        sub : sub
+    });
+
+})
+
+
 servidor.listen(
     5050,
     () => console.log('Api subiu com sucesso na porta 5050'));
