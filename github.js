83f4a0362aec93e398aa222abf8635ebf7d69363
macedorayc <macedorayane335@gmail.com>
2024-08-02 15:56:46 -0300

    foi

--- github.js
+++ github.js
@@ -2,19 +2,19 @@ import express from "express";
 
 const servidor = express();
 
-servidor.get('/git/estudo/:n1/:n2', (resp,req) => {
+servidor.get('/projeto/soma/:n1/:n2', (req, resp) => {
 
-let n1 = Number(req.params.n1)
-let n2 = Number(req.params.n2)
+    let n1 = Number(req.params.n1);
+    let n2 = Number(req.params.n2);
 
-let soma = n1 + n2
+    let soma = n1 + n2
 
 
-resp.send('A soma é ' + soma);
+    resp.send('A soma é ' + soma);
 
 })
 
 
 servidor.listen(
-    5001,
-    () => console.log('Api subiu com sucesso na porta 5001')) ;
+    5050,
+    () => console.log('Api subiu com sucesso na porta 5050'));
